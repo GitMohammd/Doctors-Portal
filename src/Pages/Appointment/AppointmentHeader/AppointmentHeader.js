e2fb1b692b9a10d7dd3e562a8223900736447171
GitMohammad <mohammadshamimreza23393@gmail.com>
2022-02-17 16:51:39 +0600

    startCalenderPart

--- src/Pages/Appointment/AppointmentHeader/AppointmentHeader.js
+++ src/Pages/Appointment/AppointmentHeader/AppointmentHeader.js
@@ -1,10 +1,22 @@
+import { Container, Grid } from '@mui/material';
+import  Box  from '@mui/system/Box';
 import React from 'react';
+import chair from '../../../images/chair.png'
+import Calender from '../Calender/Calender';
 
 const AppointmentHeader = () => {
     return (
-        <div>
-            
-        </div>
+      <Container>
+        <Box>
+          <Grid container spacing={2}>
+            <Grid item xs={6} md={8}>
+                <Calender></Calender>            </Grid>
+            <Grid item xs={6} md={4}>
+              <img style={{ width: "100%" }} src={chair} alt="" />
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
     );
 };
 
