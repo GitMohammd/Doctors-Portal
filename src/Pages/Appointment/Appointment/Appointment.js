cd93911e6068a990ba3b2b8c8a9a8752a72d9521
GitMohammad <mohammadshamimreza23393@gmail.com>
2022-02-17 16:18:49 +0600

    startAppointPart

---
+++ src/Pages/Appointment/Appointment/Appointment.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from '../../Shared/Navigation/Navigation';
+import AppointmentHeader from '../AppointmentHeader/AppointmentHeader';
+
+const Appointment = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+            <AppointmentHeader></AppointmentHeader>
+        </div>
+    );
+};
+
+export default Appointment;
