ab6623ea9b83e22d0d409ea51e89d708bfec0250
GitMohammad <mohammadshamimreza23393@gmail.com>
2022-04-21 11:27:26 +0600

    add toLocaleString

--- src/Pages/Dashboard/Appointments/Appointments.js
+++ src/Pages/Dashboard/Appointments/Appointments.js
@@ -13,7 +13,7 @@ const Appointments = ({date}) => {
     const { user, token } = UseAuth();
     const [appointments, setAppointments] = useState([])
     useEffect(() => {
-        const url = `https://fierce-dusk-81451.herokuapp.com/appointments?email=${user.email}&date=${date}`;
+        const url = `https://fierce-dusk-81451.herokuapp.com/appointments?email=${user.email}&date=${date.toLocaleDateString()}`;
         fetch(url, {
             headers:{
           authorization: `Bearer ${token}`
